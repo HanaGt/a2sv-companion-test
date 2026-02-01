9607a8dcb113c5c9e6b077b4f0db9af0173c9286
Hana Guta <163635240+HanaGt@users.noreply.github.com>
2026-02-01 04:51:53 -0800

    Solve Two Sum | Time: 56 | Attempts: 3

--- two-sum.py
+++ two-sum.py
@@ -1,18 +1,10 @@
-1
-2
-3
-4
-5
-6
-7
+1class Solution:
+2    def twoSum(self, nums: List[int], target: int) -> List[int]:
+3        arr = []
+4        for i in range(len(nums)):
+5            for j in range(i + 1 , len(nums)):
+6                if nums[i] + nums[j] == target:
+7                    arr.append((i , j))
 8
-9
+9        return arr[0]
 10
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        arr = []
-        for i in range(len(nums)):
-            for j in range(i + 1 , len(nums)):
-                if nums[i] + nums[j] == target:
-                    arr.append((i , j))
-        return arr[0]
