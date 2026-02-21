8f8850fc6c8b4cd1c6bd4f0f2f5636838e732d9d
Hana Guta <163635240+HanaGt@users.noreply.github.com>
2026-02-21 15:14:28 +0300

    Add solution for Array Splitting

--- codeforces/1197C Array-Splitting.py
+++ codeforces/1197C Array-Splitting.py
@@ -1,19 +1,4 @@
-class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
-        words = s.strip().split()
-        if len(pattern) != len(words):
-            return False
-
-        hm = {}
-        used = set()
-
-        for ch, word in zip(pattern, words):
-            if ch in hm:
-                if hm[ch] != word:
-                    return False
-            else:
-                if word in used:
-                    return False
-                hm[ch] = word
-                used.add(word)
-        return True
+R=lambda:map(int,input().split())
+n,k=R()
+a=*R(),
+print(sum(sorted(y-x for x,y in zip(a,a[1:]))[:n-k]))
