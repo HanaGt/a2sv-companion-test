8a59dffa163e6947a3dad2a26011c84d243d15c0
Hana Guta <163635240+HanaGt@users.noreply.github.com>
2026-02-21 12:57:27 +0300

    Add solution for Optimal Point on a Line

---
+++ codeforces/710B Optimal-Point on a Line.py
@@ -0,0 +1,48 @@
+Africa Time)]: Adding the channel '726b90863d05897cd6ebeec3c7d8d6922afe05a2'.
+codeforces.js?v=20160131:3 INFO [Sat Feb 21 2026 12:54:37 GMT+0300 (East Africa Time)]: Skipping the channel 'undefined'.
+codeforces.js?v=20160131:3 INFO [Sat Feb 21 2026 12:54:37 GMT+0300 (East Africa Time)]: Disconnected.
+codeforces.js?v=20160131:1622 setupContestTimes...
+codeforces.content.js:1273 [ WER: Connected to Web Extension Hot Reloader ]
+codeforces.js?v=20160131:3 INFO [Sat Feb 21 2026 12:54:38 GMT+0300 (East Africa Time)]: Connected!
+codeforces.content.js:1669 [A2SV Codeforces] Submit from row {submissionId: '363745112', timeTaken: '7'}
+codeforces.content.js:1616 [A2SV Codeforces] view-source clicked {submissionId: '363745112', preFilledTime: true}
+codeforces.content.js:1554 [A2SV Codeforces] pushSubmission started {submissionId: '363745112', timeTaken: '7'}
+codeforces.content.js:1567 [A2SV Codeforces] getSubmissionDetail done {questionUrl: 'https://codeforces.com/contest/710/problem/B', codeLength: 3342}
+codeforces.content.js:1568 [A2SV Codeforces] code to push: [A2SV Codeforces Service] push called {codeforcesHandle: 'Hana-Guta', submissionId: 363745112, problemName: 'Optimal Point on a Line', timeTaken: '7', codeLength: 0}codeLength: 0codeforcesHandle: "Hana-Guta"problemName: "Optimal Point on a Line"submissionId: 363745112timeTaken: "7"[[Prototype]]: Object
+codeforces.service.ts:30 [A2SV Codeforces Service] storage {selectedRepo: 'a2sv-companion-test', folderPath: '', studentName: 'Hana Guta', group: 'G71'}
+codeforces.service.ts:53 [A2SV Codeforces Service] uploading to GitHub {path: 'codeforces/710B Optimal-Point on a Line.py', commitMsg: 'Add solution for Optimal Point on a Line'}
+codeforces.service.ts:54 [A2SV Codeforces Service] submission code for GitHub: 
+codeforces.service.ts:93 [A2SV Codeforces Service] PUSH_SUBMISSION_TO_SHEETS received
+codeforces.service.ts:18 [A2SV Codeforces Service] push called {codeforcesHandle: 'Hana-Guta', submissionId: 363745112, problemName: 'Optimal Point on a Line', timeTaken: '7', codeLength: 0}
+codeforces.service.ts:30 [A2SV Codeforces Service] storage {selectedRepo: 'a2sv-companion-test', folderPath: '', studentName: 'Hana Guta', group: 'G71'}
+codeforces.service.ts:53 [A2SV Codeforces Service] uploading to GitHub {path: 'codeforces/710B Optimal-Point on a Line.py', commitMsg: 'Add solution for Optimal Point on a Line'}
+codeforces.service.ts:54 [A2SV Codeforces Service] submission code for GitHub: 
+codeforces.service.ts:58 [A2SV Codeforces Service] GitHub upload done {gitUrl: 'https://github.com/HanaGt/a2sv-companion-test/blob…odeforces/710B%20Optimal-Point%20on%20a%20Line.py'}
+codeforces.service.ts:62 [A2SV Codeforces Service] getTries done {tries: 1}
+index.ts:26 A2SV submit (sent JSON): {
+  "group": "G71",
+  "student_full_name": "Hana Guta",
+  "problem_url": "https://codeforces.com/contest/710/problem/B",
+  "github_link": "https://github.com/HanaGt/a2sv-companion-test/blob/main/codeforces/710B%20Optimal-Point%20on%20a%20Line.py",
+  "attempts": "1",
+  "time": "7"
+}
+index.ts:27 A2SV submit (calling Apps Script): https://script.google.com/macros/s/AKfycbwNulp3XsBOlHEfpUU0WAZ-IkqOW3dNDBzchtZJzmx1UczJRT_mtcvXFlKQZbmkRr3qLw/exec
+codeforces.service.ts:58 [A2SV Codeforces Service] GitHub upload done {gitUrl: 'https://github.com/HanaGt/a2sv-companion-test/blob…odeforces/710B%20Optimal-Point%20on%20a%20Line.py'}
+codeforces.service.ts:62 [A2SV Codeforces Service] getTries done {tries: 1}
+index.ts:26 A2SV submit (sent JSON): {
+  "group": "G71",
+  "student_full_name": "Hana Guta",
+  "problem_url": "https://codeforces.com/contest/710/problem/B",
+  "github_link": "https://github.com/HanaGt/a2sv-companion-test/blob/main/codeforces/710B%20Optimal-Point%20on%20a%20Line.py",
+  "attempts": "1",
+  "time": "7"
+}
+index.ts:27 A2SV submit (calling Apps Script): https://script.google.com/macros/s/AKfycbwNulp3XsBOlHEfpUU0WAZ-IkqOW3dNDBzchtZJzmx1UczJRT_mtcvXFlKQZbmkRr3qLw/exec
+index.ts:35 A2SV submit (Apps Script response): 200  {"status":"success","message":"Updated successfully."}
+codeforces.service.ts:71 [A2SV Codeforces Service] pushToSheet result {success: true, message: 'Updated successfully.'}
+index.ts:35 A2SV submit (Apps Script response): 200  {"status":"success","message":"Updated successfully."}
+codeforces.service.ts:71 [A2SV Codeforces Service] pushToSheet result {success: true, message: 'Updated successfully.'}
+codeforces.content.js:1572 [A2SV Codeforces] getSubmission done {problemName: 'Optimal Point on a Line'}
+codeforces.content.js:1585 [A2SV Codeforces] push result {success: false, message: "Failed to execute 'btoa' on 'WorkerGlobalScope': T… contains characters outside of the Latin1 range."}message: "Failed to execute 'btoa' on 'WorkerGlobalScope': The string to be encoded contains characters outside of the Latin1 range."success: false[[Prototype]]: Object
+css-generic.js:1 uBOL: Generic cosmetic filtering stopped because no more DOM changes
