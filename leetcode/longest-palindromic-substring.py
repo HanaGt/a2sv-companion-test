d6fd4d64c58915e9f51f04045a40b9712a97cc97
Hana Guta <163635240+HanaGt@users.noreply.github.com>
2026-02-22 14:39:59 +0300

    Add solution for Longest Palindromic Substring

---
+++ leetcode/longest-palindromic-substring.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand_around_center(left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        if len(s) <= 1:
+            return s
+
+        longest_palindrome = ""
+
+        for i in range(len(s)):
+          
+            palindrome_odd = expand_around_center(i, i)
+            if len(palindrome_odd) > len(longest_palindrome):
+                longest_palindrome = palindrome_odd
+
+            palindrome_even = expand_around_center(i, i + 1)
+            if len(palindrome_even) > len(longest_palindrome):
+                longest_palindrome = palindrome_even
+
+        return longest_palindrome
+
+
